478a18b7c59b8d9b0a318bfce992dfcd930d30f5
Saish Mhatre <saish.mhatre26@gmail.com>
2020-01-21 06:50:00 +0530

    updated solidity file

--- contracts/PatientBlock.sol
+++ contracts/PatientBlock.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.10;
 
 contract PatientBlock {
 
-    event NewPatient(uint PatientId, string f_name);
+  
     struct Patient {
         string f_name;
         string m_name;
@@ -12,18 +12,14 @@ contract PatientBlock {
         uint age;        
         uint mobile;
     }
-    Patient[] public patients;
-    mapping(uint => Patient) patientsAcc;
+    Patient patients;
+    
 
     function setPatientData(string memory _f_name, string memory _m_name, string memory _l_name, string memory _p_address, string memory _gender, uint _age, uint _mobile) public {
-          uint id = patients.push(Patient(_f_name, _m_name, _l_name, _p_address, _gender, _age, _mobile)) - 1;
-            
-       // PatientToOwner[id] = msg.sender;
-        emit NewPatient(id, _f_name);
+        patients = Patient(_f_name, _m_name, _l_name, _p_address, _gender, _age, _mobile);
     }
 
-    //function getPatientData() public view returns(address[]){
-
-   // }
-    
+    function getPatientData() public view returns(string memory, string memory, string memory, string memory, string memory, uint, uint){
+        return(patients.f_name, patients.m_name, patients.l_name, patients.p_address, patients.gender, patients.age, patients.mobile);
+    }    
 }
